cb0a66ff3cda0007041dec8c8c50d07c54753439
SOURABH <dubeysourabh2001@gmail.com>
2021-05-18 00:45:00 +0530

    JSConcepts

---
+++ Module1/JSConcept/executionContext.js
@@ -0,0 +1,12 @@
+console.log("start");
+var name = "Steve";
+
+function getName(name){
+    return name + " Rogers";
+}
+
+var fullName = getName("Steve");
+var newName = getName("Tony");
+
+console.log(fullName);
+console.log(newName);
